5db9f7abdd0ac6b2406190b8aff6d28bf35e3749
steven rogers <me@srogers.net>
2020-12-28 23:08:25 -0500

    Add development workflows.
    1. build:watch
      - Watch for file system changes and rebuild the delta for a new bundle.
    2. build:serve
      - Start a webpack-dev-server hosted on localhost with hot reloading enabled.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,20 @@
 const path = require('path')
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        hot: true,
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        })
+    ],
 })
